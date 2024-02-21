6419d5abb0d26561b9c6db8c8ce9d47f04b3355c
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-02-21 05:37:52 -0500

    activating strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+/* These are reserved kewords so 'use strict' 
+    or strict mode will not allow these reservered keywords */
+// const interface = 'Audio';
+// const private = 2;
